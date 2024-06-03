74624dfb1b634428b8966a9308e141a51627d4aa
KrzysiekR-00 <krzysztof.rygol@gmail.com>
2024-06-03 08:47:44 +0200

    Add udp client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,19 +1,44 @@
-﻿using System.Net.Sockets;
-using System.Net;
+﻿using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
-var ipEndPoint = IPEndPoint.Parse("127.0.0.1");
-ipEndPoint.Port = 13;
+{
+    var ipEndPoint = IPEndPoint.Parse("127.0.0.1");
+    ipEndPoint.Port = 13;
 
-using TcpClient client = new();
-await client.ConnectAsync(ipEndPoint);
-await using NetworkStream stream = client.GetStream();
+    using TcpClient client = new();
+    await client.ConnectAsync(ipEndPoint);
+    await using NetworkStream stream = client.GetStream();
 
-var buffer = new byte[1_024];
-int received = await stream.ReadAsync(buffer);
+    var buffer = new byte[1_024];
+    int received = await stream.ReadAsync(buffer);
 
-var message = Encoding.UTF8.GetString(buffer, 0, received);
-Console.WriteLine(DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.fff") + " - Message received:");
-Console.WriteLine(message);
+    var message = Encoding.UTF8.GetString(buffer, 0, received);
+    Console.WriteLine(DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.fff") + " - Message received:");
+    Console.WriteLine(message);
+}
+
+//
+
+await Task.Delay(500);
+Console.WriteLine("");
+
+//
+
+{
+    Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+    IPAddress broadcast = IPAddress.Parse("127.0.0.1");
+
+    string message = "Udp messsage " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.fff");
+    byte[] sendbuf = Encoding.UTF8.GetBytes(message);
+    IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+
+    s.SendTo(sendbuf, ep);
+
+    Console.WriteLine("Message sent");
+}
+
+//
 
 Console.ReadKey();
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,27 +1,66 @@
-﻿using System.Net.Sockets;
-using System.Net;
+﻿using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 var ipEndPoint = new IPEndPoint(IPAddress.Any, 13);
-TcpListener listener = new(ipEndPoint);
 
-try
 {
-    listener.Start();
+    TcpListener listener = new(ipEndPoint);
 
-    using TcpClient handler = await listener.AcceptTcpClientAsync();
-    await using NetworkStream stream = handler.GetStream();
+    try
+    {
+        listener.Start();
 
-    var message = "Message " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.fff");
-    var dateTimeBytes = Encoding.UTF8.GetBytes(message);
-    await stream.WriteAsync(dateTimeBytes);
+        using TcpClient handler = await listener.AcceptTcpClientAsync();
+        await using NetworkStream stream = handler.GetStream();
 
-    Console.WriteLine("Message sent:");
-    Console.WriteLine(message);
+        var message = "Tcp message " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.fff");
+        var dateTimeBytes = Encoding.UTF8.GetBytes(message);
+        await stream.WriteAsync(dateTimeBytes);
+
+        Console.WriteLine("Message sent");
+        //Console.WriteLine("Message sent:");
+        //Console.WriteLine(message);
+    }
+    finally
+    {
+        listener.Stop();
+    }
 }
-finally
+
+//
+
+Console.WriteLine("");
+
+//
+
 {
-    listener.Stop();
+    int listenPort = 11000;
+    UdpClient listener = new UdpClient(listenPort);
+    IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+    try
+    {
+        //while (true)
+        {
+            Console.WriteLine("Waiting for broadcast");
+            byte[] bytes = listener.Receive(ref groupEP);
+
+            Console.WriteLine(DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.fff") + " - Message received:");
+            Console.WriteLine($"Received broadcast from {groupEP}:");
+            Console.WriteLine(Encoding.UTF8.GetString(bytes, 0, bytes.Length));
+        }
+    }
+    catch (SocketException e)
+    {
+        Console.WriteLine(e);
+    }
+    finally
+    {
+        listener.Close();
+    }
 }
+
+//
 
 Console.ReadKey();
