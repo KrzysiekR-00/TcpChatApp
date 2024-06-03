3d155739c415b4c7255800f00e9d5913d341cd3b
KrzysiekR-00 <krzysztof.rygol@gmail.com>
2024-06-03 08:15:34 +0200

    Add server and client projects

---
+++ Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+var ipEndPoint = IPEndPoint.Parse("127.0.0.1");
+ipEndPoint.Port = 13;
+
+using TcpClient client = new();
+await client.ConnectAsync(ipEndPoint);
+await using NetworkStream stream = client.GetStream();
+
+var buffer = new byte[1_024];
+int received = await stream.ReadAsync(buffer);
+
+var message = Encoding.UTF8.GetString(buffer, 0, received);
+Console.WriteLine($"Message received: \"{message}\"");
+// Sample output:
+//     Message received: "📅 8/22/2022 9:07:17 AM 🕛"
+
+Console.ReadKey();
---
+++ Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+var ipEndPoint = new IPEndPoint(IPAddress.Any, 13);
+TcpListener listener = new(ipEndPoint);
+
+try
+{
+    listener.Start();
+
+    using TcpClient handler = await listener.AcceptTcpClientAsync();
+    await using NetworkStream stream = handler.GetStream();
+
+    var message = $"📅 {DateTime.Now} 🕛";
+    var dateTimeBytes = Encoding.UTF8.GetBytes(message);
+    await stream.WriteAsync(dateTimeBytes);
+
+    Console.WriteLine($"Sent message: \"{message}\"");
+    // Sample output:
+    //     Sent message: "📅 8/22/2022 9:07:17 AM 🕛"
+}
+finally
+{
+    listener.Stop();
+}
+
+Console.ReadKey();
