ad712b3faa710e254ab8de236865656c57a7fe29
KrzysiekR-00 <krzysztof.rygol@gmail.com>
2024-06-07 15:40:02 +0200

    Implement messages sending in wpf app

---
+++ WpfClient/Models/ChatClient.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WpfClient.Models
+{
+    internal class ChatClient
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+
+        internal bool IsInitialized { get; private set; }
+
+        internal ChatClient() 
+        {
+        
+        }
+
+        ~ChatClient()
+        {
+            stream.Dispose();
+            client.Dispose();
+        }
+
+        internal async Task Initialize()
+        {
+            IPEndPoint iPEndPoint = IPEndPoint.Parse("127.0.0.1");
+            iPEndPoint.Port = 13000;
+
+            //using TcpClient client = new();
+            client = new();
+            await client.ConnectAsync(iPEndPoint);
+            //await using NetworkStream stream = client.GetStream();
+            
+            stream = client.GetStream();
+
+            IsInitialized = true;
+        }
+
+        internal async Task<string> Receive()
+        {
+            var buffer = new byte[1_024];
+            int received = await stream.ReadAsync(buffer);
+
+            //var message = Encoding.UTF8.GetString(buffer, 0, received);
+            //Console.WriteLine($"{DateTime.Now} Message received:\r\n{message}");
+
+            return Encoding.UTF8.GetString(buffer, 0, received);
+        }
+
+        internal async Task Send(string message)
+        {
+            var bytes = Encoding.UTF8.GetBytes(message);
+            await stream.WriteAsync(bytes);
+        }
+    }
+}
--- WpfClient/ViewModels/MainWindowViewModel.cs
+++ WpfClient/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System.Windows.Input;
+using WpfClient.Models;
 
 namespace WpfClient.ViewModels
 {
     internal class MainWindowViewModel : ViewModelBase
     {
+        private ChatClient _chatClient = new();
+
         private string _conversation;
         private string _messageToSend;
 
@@ -34,11 +37,22 @@ namespace WpfClient.ViewModels
             _conversation = string.Empty;
             _messageToSend = string.Empty;
             SendMessageCommand = new RelayCommand(_ => SendMessage());
+
+            Initialize();
+        }
+
+        private async Task Initialize()
+        {
+            await _chatClient.Initialize();
+            _conversation = await _chatClient.Receive();
         }
 
         private void SendMessage()
         {
-            Conversation += MessageToSend + "\r\n";
+            //Conversation += MessageToSend + "\r\n";
+            //MessageToSend = string.Empty;
+
+            _chatClient.Send(MessageToSend);
             MessageToSend = string.Empty;
         }
     }
